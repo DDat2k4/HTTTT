c4ab7fcb32737d32e17ad2dff2059fbfd7040a68
Dinh Dat <tddat19102004@gmail.com>
2025-05-31 10:30:18 +0700

    update

--- cong-nghe-web-dattestbr/src/main/java/com/HTTN/thitn/controller/AnswerController.java
+++ cong-nghe-web-dattestbr/src/main/java/com/HTTN/thitn/controller/AnswerController.java
@@ -1,7 +1,9 @@
 package com.HTTN.thitn.controller;
 
+import com.HTTN.thitn.dto.Response.AnswerResponse;
 import com.HTTN.thitn.entity.Answer;
 import com.HTTN.thitn.entity.User;
+import com.HTTN.thitn.mapper.AnswerResponseMapper;
 import com.HTTN.thitn.security.CustomUserDetails;
 import com.HTTN.thitn.service.AnswerService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,23 +12,45 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/student/answers")
 public class AnswerController {
+
     @Autowired
     private AnswerService answerService;
     // gửi đáp án cho từng câu hỏi
     @PostMapping("/submission/{submissionId}/question/{questionId}/choice/{choiceId}")
-    public ResponseEntity<Answer> createAnswer(@PathVariable Integer submissionId,
-                                               @PathVariable Integer questionId,
-                                               @PathVariable Integer choiceId) {
+    public ResponseEntity<AnswerResponse> createAnswer(@PathVariable Integer submissionId,
+                                                       @PathVariable Integer questionId,
+                                                       @PathVariable Integer choiceId) {
 
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         CustomUserDetails userDetails = (CustomUserDetails) authentication.getPrincipal();
         User user = userDetails.getUser();
 
         Answer answer = answerService.createAnswer(submissionId, questionId, choiceId, user);
-        return ResponseEntity.status(201).body(answer);
+        AnswerResponse response = AnswerResponseMapper.toResponse(answer);
+        return ResponseEntity.status(201).body(response);
     }
+
+    //gửi đáp cho câu hoi nhiều lựa chọn
+    @PostMapping("/submission/{submissionId}/question/{questionId}/choices")
+    public ResponseEntity<AnswerResponse> createMultiChoiceAnswer(
+            @PathVariable Integer submissionId,
+            @PathVariable Integer questionId,
+            @RequestBody List<Integer> choiceIds) {
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        CustomUserDetails userDetails = (CustomUserDetails) authentication.getPrincipal();
+        User user = userDetails.getUser();
+
+        Answer answer = answerService.createMultiChoiceAnswer(submissionId, questionId, choiceIds, user);
+        AnswerResponse response = AnswerResponseMapper.toResponse(answer);
+        return ResponseEntity.status(201).body(response);
+    }
+
+
 
 }
